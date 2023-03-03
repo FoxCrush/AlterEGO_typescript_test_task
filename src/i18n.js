784611d0a29910afef9a9889f3f03299a63f8d35
FoxCrush <foxteryr@gmail.com>
2023-03-04 00:35:12 +0200

    attempt to fix ghpages transate

--- src/i18n.js
+++ src/i18n.js
@@ -8,6 +8,7 @@ i18next
   .use(I18nextBrowserLanguageDetector)
   .use(Backend)
   .init({
-    // debug: true,
+    debug: true,
     fallbackLng: "en",
+    loadPath: "/AlterEGO_typescript_test_task/locales/{{lng}}/{{ns}}.json",
   });
